0e9858120bd51a6cfa08ff38af0d5f03c91b39e1
Harshit Poddar <89685284+Harshit-Poddar90@users.noreply.github.com>
2021-11-19 16:32:17 +0530

    Create The_No_Guessing_Game.py

---
+++ The No Guessing Game/The_No_Guessing_Game.py
@@ -0,0 +1,33 @@
+inp1= input("Welcome to 'The Guessing Number Game!!!'\n"
+            "Press Enter to move forward\n")
+
+print("Do you want to start the game?\n"
+      "Press 1 to start else 2 to quit the game")
+inp2= int(input())
+
+if inp2==2:
+   print("You quit the game")
+
+if inp2==1:
+    print("Let's start the game")
+
+    n = 18
+    i = 0
+    while (i < 9):
+        i = i + 1
+        print("Enter a number")
+        x = int(input())
+        if x == n:
+            print("Congo!! ur number matched")
+            print("number guesses u took", i)
+            break
+        elif x < 18:
+            print("Try again with a larger number")
+            print("Chances left", 9 - i)
+            continue
+        elif x > 18:
+            print("Try again with a smaller number")
+            print("chances left", 9 - i)
+        elif i > 9:
+            print("OOPS!! U have used all ur chances")
+            break
